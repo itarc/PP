178d839ea1ee9c6fb70af8c6ba98f2752c29dcef
itarc <itarc@numericable.fr>
2013-12-02 16:52:03 +0100

    cleaning 1 attendee unittests for poll model

--- spec/models/1-question-with-0-attendee_unittests.rb
+++ spec/models/1-question-with-0-attendee_unittests.rb
@@ -7,8 +7,8 @@ require "test/unit"
 $user_id_1 = "user_1"
 $user_id_2 = "user_2"
 $user_id_3 = "user_3"
-$question_id_1_1 = "Unix est il un système ?"
-$question_id_1_2 = "Unix est il réparti ?"
+$question_id_1 = "Unix est il un système ?"
+$question_id_2 = "Unix est il réparti ?"
 $answer_1 = "Oui"
 $answer_2 = "Non"
 
@@ -16,7 +16,7 @@ class TestPollQuestionWithNoResponseGiven < Test::Unit::TestCase
 	  
   def setup
     $db.execute_sql("delete from polls")
-    $poll_question = PollQuestion.new(    $question_id_1)
+    $poll_question = PollQuestion.new($question_id_1)
   end
 
   def test00
@@ -33,15 +33,15 @@ class TestPollQuestionWithNoResponseGiven < Test::Unit::TestCase
   end
 
   def test03
-    assert_equal 0, $poll_question.total_number_of_users_for(    $answer_1)
+    assert_equal 0, $poll_question.total_number_of_users_for( $answer_1)
   end
 
   def test04
-    assert_equal 0, $poll_question.rate_for(    $answer_1)
+    assert_equal 0, $poll_question.rate_for( $answer_1)
   end    
 
   def test05
-    expected = {     $question_id_1 => { } }
+    expected = { $question_id_1 => { } }
     assert_equal expected, $poll_question.rates
   end
 
