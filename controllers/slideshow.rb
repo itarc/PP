91a3d72812044869fcdb81e34256506c408681f8
itarc <itarc@numericable.fr>
2014-01-20 13:05:36 +0100

    seperate gets and posts routes

--- controllers/slideshow.rb
+++ controllers/slideshow.rb
@@ -9,6 +9,10 @@ enable :sessions; set :session_secret, 'secret'
 
 require_relative 'slideshow_helper'
 
+# ---------
+# GETs
+# ---------
+
 get '/' do
   session[:user_id] ||= next_id
   redirect "slideshow-attendee.html"
@@ -18,16 +22,20 @@ get '/teacher-x1973' do
   redirect "slideshow-teacher.1973x.html"
 end
 
-post '/teacher_current_slide' do
-  update_current_slide_id params[:index]
-end
-
 get '/teacher_current_slide' do
   current_slide_id
 end
 
 get '/poll_response_*_rate_to_*' do
   PollQuestion.new(question_id).rate_for(answer).to_s
+end
+
+# ---------
+# POSTs
+# ---------
+
+post '/teacher_current_slide' do
+  update_current_slide_id params[:index]
 end
 
 post '/poll_response_*_to_*' do
@@ -46,4 +54,3 @@ post '/code_execution_result' do
   code = request.env["rack.input"].read
   run_ruby(code)
 end
-
