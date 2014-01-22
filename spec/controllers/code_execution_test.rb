9ebd057c5f279a9a7cf35d19ece047bbbc534d4a
itarc <itarc@numericable.fr>
2014-01-22 10:08:52 +0100

    creating an testing eval_ruby and run_ruby route

--- spec/controllers/code_execution_test.rb
+++ spec/controllers/code_execution_test.rb
@@ -5,7 +5,7 @@ require 'rack/test'
 
 disable :sessions # Mandatory to test sessions, otherwise we cannot access session object
 
-class TestCoding < Test::Unit::TestCase
+class TestCodeEvaluation < Test::Unit::TestCase
   
   include Rack::Test::Methods
 
@@ -13,33 +13,59 @@ class TestCoding < Test::Unit::TestCase
     Sinatra::Application
   end
   
-  def setup
-    $db.execute_sql("update compteur set identifiant = 0")
-  end  
-  
-  def test01_should_not_execute_anything
-    post '/code_execution_result', ""
+  def test01_should_not_evaluate_anything_when_no_code
+    post '/code_evaluation_result', ""
     assert_equal "", last_response.body
   end
   
-  def test02_should_execute_an_evaluation
-    post '/code_execution_result', "1"
+  def test02_should_evaluate_an_expression
+    post '/code_evaluation_result', "1"
     assert_equal "1", last_response.body	  
   end
   
-  def test03_should_evaluate_an_arythmetic_operation
-    post '/code_execution_result', "2 + 2"
+  def test03_should_evaluate_another_expression
+    post '/code_evaluation_result', "2 + 2"
     assert_equal "4", last_response.body
   end  
   
-  def test04_should_execute_an_instruction
-    post '/code_execution_result', "print 3"
+  def test04_should_evaluate_an_instruction
+    post '/code_evaluation_result', "print 3"
     assert_equal "3", last_response.body	  
   end
   
   def test05_should_raise_an_error
-    post '/code_execution_result', "puts a"
+    post '/code_evaluation_result', "puts a"
     assert last_response.body.include?("undefined local variable or method `a'"), last_response.errors 
   end 
 
+end
+
+class TestCodeRun < Test::Unit::TestCase
+  
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+  
+  def test01_should_not_run_anything_when_no_code
+    post '/code_run_result', ""
+    assert_equal "", last_response.body
+  end
+  
+  def test02_should_run_code
+    post '/code_run_result', "print 2"
+    assert_equal "2", last_response.body
+  end
+
+  def test03_should_catch_exception
+    post '/code_run_result', "print A"
+    assert last_response.body.include?("uninitialized constant A")
+  end
+  
+  def test04_should_run_unit_tests
+    post '/code_run_result', "require 'test/unit'"
+    assert last_response.body.include?("0 tests, 0 assertions, 0 failures, 0 errors, 0 skips")  
+  end
+  
 end
