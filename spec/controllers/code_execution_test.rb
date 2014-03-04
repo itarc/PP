ceacac35c52f277b0ab8ec63178d86d320d00e72
itarc <itarc@numericable.fr>
2014-03-04 15:34:25 +0100

    remove code evaluation (evaluate)

--- spec/controllers/code_execution_test.rb
+++ spec/controllers/code_execution_test.rb
@@ -5,41 +5,6 @@ require_relative "../../controllers/slideshow"
 require 'test/unit'
 require 'rack/test'
 
-class TestCodeEvaluation < Test::Unit::TestCase
-  
-  include Rack::Test::Methods
-
-  def app
-    Sinatra::Application
-  end
-  
-  def test01_should_not_evaluate_anything_when_no_code
-    post '/code_evaluation_result', ""
-    assert_equal "", last_response.body
-  end
-  
-  def test02_should_evaluate_an_expression
-    post '/code_evaluation_result', "1"
-    assert_equal "1", last_response.body	  
-  end
-  
-  def test03_should_evaluate_another_expression
-    post '/code_evaluation_result', "2 + 2"
-    assert_equal "4", last_response.body
-  end  
-  
-  def test04_should_evaluate_an_instruction
-    post '/code_evaluation_result', "print 3"
-    assert_equal "3", last_response.body	  
-  end
-  
-  def test05_should_raise_an_error
-    post '/code_evaluation_result', "puts a"
-    assert last_response.body.include?("undefined local variable or method `a'"), last_response.errors 
-  end 
-
-end
-
 class TestCodeRun < Test::Unit::TestCase
   
   include Rack::Test::Methods
--- spec/models/RunTime_test.rb
+++ spec/models/RunTime_test.rb
@@ -3,34 +3,6 @@
 require_relative '../../models/RunTime'
 require 'test/unit'
 
-class TestRunTime_eval_ruby < Test::Unit::TestCase
-
-  def test01_should_be_empty_when_no_code
-    assert_equal "", eval_ruby("")
-  end
-  
-  def test02_should_evaluate_expression
-    assert_equal "1", eval_ruby("1")
-  end
-
-  def test03_should_evaluate_another_expression
-    assert_equal "2", eval_ruby("1 + 1")
-  end
-  
-  def test04_should_evaluate_an_instruction
-    assert_equal "3", eval_ruby("print 3")
-  end
-  
-  def test05_should_evaluate_another_instruction
-    assert_equal "4\n", eval_ruby("puts 4")
-  end
-  
-  def test06_should_return_error_message
-    assert eval_ruby("puts a").include?("undefined local variable or method `a'")
-  end
-
-end
-
 class TestRunTime_run_ruby < Test::Unit::TestCase
   
   def test01_should_be_empty_when_no_code
