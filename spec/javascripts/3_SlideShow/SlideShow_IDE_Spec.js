04bf9596dc05acbfb7a47b6646609c84e98b3b82
itarc <itarc@numericable.fr>
2014-04-07 12:08:30 +0200

    slide show IDE spec little cleaning

--- spec/javascripts/3_SlideShow/SlideShow_IDE_Spec.js
+++ spec/javascripts/3_SlideShow/SlideShow_IDE_Spec.js
@@ -5,35 +5,23 @@ describe("SlideShow with an IDE Slide", function() {
     setFixtures("<div class='slides'><div class='slide'><div id='code_input'><div id='execute'><div id='send_code'/><div id='get_code'/><div id='code_output'><div class='slide'/></div>")	  
     spyOn(CodeSlide.prototype, '_update');    
     
-  });  
+  });
 
-  //~ it("should be updated when first slide", function() {
-
-    //~ getResource = jasmine.createSpy('getResource').andReturn('0');    
-
-    //~ var slideShow = new SlideShow(queryAll(document, '.slide'))
-
-    //~ expect(CodeSlide.prototype._update.calls.length).toBe(1);     	  
-
-  //~ });
-
-  it("should be updated when refreshed", function() {
+  it("should be updated when refreshed and position changed", function() {
  
     var slideShow = new SlideShow(queryAll(document, '.slide'))
 
-    //~ expect(CodeSlide.prototype._update.calls.length).toBe(1);    
     expect(slideShow.position._currentIndex).toBe(0);     
          
     getResource = jasmine.createSpy('getResource').andReturn('11');
     
     slideShow._refresh();
    
-    //~ expect(CodeSlide.prototype._update.calls.length).toBe(2);
     expect(CodeSlide.prototype._update.calls.length).toBe(1);
  
    });
 
-  it("should NOT be updated when refreshed but nothing changed", function() {
+  it("should NOT be updated when refreshed but position did not change", function() {
 
     getResource = jasmine.createSpy('getResource').andReturn('0;false');
 
