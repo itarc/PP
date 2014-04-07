2610eb351bf2a67c4972b96a126dafb81ee860e6
itarc <itarc@numericable.fr>
2014-04-07 15:14:46 +0200

    Blackboard refresh little refactoring

--- spec/javascripts/6_Blackboard/Blackboard_Refresh_Spec.js
+++ spec/javascripts/6_Blackboard/Blackboard_Refresh_Spec.js
@@ -1,6 +1,6 @@
 describe("Balckboard Refresh", function() {
   
-  it("should get last Teacher run", function() {
+  it("should get last Teacher run when refreshed", function() {
     
     setFixtures("<div class='slides'><div class='slide'><div id='code_input'><div class='code_helper'><div class='code_helper'><div id='execute'><div id='send_code'/><div id='get_code'/><div id='code_output'><div class='slide'/></div>")	      
     
