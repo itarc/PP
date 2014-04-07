80f3feaed9c8d04084eccb868a59adc84e5bf0c4
itarc <itarc@numericable.fr>
2014-04-07 12:05:02 +0200

    Position spec little refactoring

--- spec/javascripts/3_SlideShow/SlideShow_Position_Spec.js
+++ spec/javascripts/3_SlideShow/SlideShow_Position_Spec.js
@@ -1,18 +1,61 @@
-describe("SlideShow Server", function() {
+describe("Slide Position", function() {
   
-  it("should leave default values if current position unknown", function() {
+  it("should get teacher current position when synchronised", function() {
 	  
     var position = new Position();
 
-    expect(position._currentIndex).toBe(0);
-    expect(position._IDEDisplayed).toBe(false);    
-    
-    getResource = jasmine.createSpy('getResource').andReturn('UNKNOWN');
+    getResource = jasmine.createSpy('getResource').andReturn('1;false');  
     
     position._synchronise();
 
+    expect(position._currentIndex).toBe(1);
+    expect(position._IDEDisplayed).toBe(false);
+
+  });  
+
+  it("should post slideshow current position", function() {
+	  
+    var position = new Position();
+    
+    postResource = jasmine.createSpy('postResource');  
+
+    position._currentIndex = 5;
+    position._IDEDisplayed = true     
+
+    position.postCurrentIndex();
+	  
+    expect(postResource.calls.length).toBe(1);
+    expect(postResource).toHaveBeenCalledWith('/teacher_current_slide', 'index=' + '5' + '&' + 'ide_displayed=' + true, ASYNCHRONOUS);
+
+  });  
+  
+  it("should NOT be update when position is unknown", function() {
+	  
+    var position = new Position();
+    
     expect(position._currentIndex).toBe(0);
     expect(position._IDEDisplayed).toBe(false);
+
+    getResource = jasmine.createSpy('getResource').andReturn('UNKNOWN;UNKNOWN');
+    
+    position._synchronise();    
+
+    expect(position._currentIndex).toBe(0);
+    expect(position._IDEDisplayed).toBe(false);
+    
+    getResource = jasmine.createSpy('getResource').andReturn('UNKNOWN;true');
+    
+    position._synchronise();    
+
+    expect(position._currentIndex).toBe(0);
+    expect(position._IDEDisplayed).toBe(false); 
+
+    getResource = jasmine.createSpy('getResource').andReturn('0;UNKNOWN');
+    
+    position._synchronise();    
+
+    expect(position._currentIndex).toBe(0);
+    expect(position._IDEDisplayed).toBe(false);    
 
   });
   
@@ -49,65 +92,6 @@ describe("SlideShow Server", function() {
 
     expect(position.hasChanged()).toBe(true);     
 
-  });  
-  
-  it("should get teacher current position when synchronised", function() {
-	  
-    var position = new Position();
-
-    getResource = jasmine.createSpy('getResource').andReturn('1;false');  
-    
-    position._synchronise();
-
-    expect(position._currentIndex).toBe(1);
-    expect(position._IDEDisplayed).toBe(false);
-
-  });  
-
-  it("should post slideshow current position", function() {
-	  
-    var position = new Position();
-    
-    postResource = jasmine.createSpy('postResource');  
-
-    position._currentIndex = 5;
-    position._IDEDisplayed = true     
-
-    position.postCurrentIndex();
-	  
-    expect(postResource.calls.length).toBe(1);
-    expect(postResource).toHaveBeenCalledWith('/teacher_current_slide', 'index=' + '5' + '&' + 'ide_displayed=' + true, ASYNCHRONOUS);
-
-  });  
-  
-  it("should NOT return current teacher position when position is unknown", function() {
-	  
-    var position = new Position();
-    
-    expect(position._currentIndex).toBe(0);
-    expect(position._IDEDisplayed).toBe(false);
-
-    getResource = jasmine.createSpy('getResource').andReturn('UNKNOWN;UNKNOWN');
-    
-    position._synchronise();    
-
-    expect(position._currentIndex).toBe(0);
-    expect(position._IDEDisplayed).toBe(false);
-    
-    getResource = jasmine.createSpy('getResource').andReturn('UNKNOWN;true');
-    
-    position._synchronise();    
-
-    expect(position._currentIndex).toBe(0);
-    expect(position._IDEDisplayed).toBe(false); 
-
-    getResource = jasmine.createSpy('getResource').andReturn('0;UNKNOWN');
-    
-    position._synchronise();    
-
-    expect(position._currentIndex).toBe(0);
-    expect(position._IDEDisplayed).toBe(false);    
-
-  });
+  });   
 
 });
