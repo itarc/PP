c7a6026ca93589c271f02efdea3d97b7d04ad845
itarc <itarc@numericable.fr>
2014-06-11 12:03:10 +0200

    javascript spec common refactoring

--- spec/javascripts/1_Common/Common_Spec.js
+++ spec/javascripts/1_Common/Common_Spec.js
@@ -1,38 +1,3 @@
-describe("Common slide list", function() {
-
-  it("should create a list of one slide", function() {
-	  
-    setFixtures("<div class='slide'></div>")
-
-    var slideList = queryAll(document, '.slide')
-	  
-    expect(Object.prototype.toString.apply(slideList)).toBe('[object Array]')
-    expect(slideList.length).toBe(1)
-    expect(Object.prototype.toString.apply(slideList[0])).toBe('[object HTMLDivElement]')
-    expect(slideList[0].tagName).toBe('DIV')
-    expect(slideList[0]).toHaveClass('slide')
-	  
-  })
-
-  it("should create a list of two slides", function() {
-	  
-    setFixtures("<div class='slide'></div><div class='slide'></div>")
-	  
-    var slideList = queryAll(document, '.slide')
-
-    expect(Object.prototype.toString.apply(slideList)).toBe('[object Array]')
-    expect(slideList.length).toBe(2)
-    expect(Object.prototype.toString.apply(slideList[0])).toBe('[object HTMLDivElement]')
-    expect(slideList[0].tagName).toBe('DIV')
-    expect(slideList[0]).toHaveClass('slide')
-    expect(Object.prototype.toString.apply(slideList[1])).toBe('[object HTMLDivElement]')
-    expect(slideList[1].tagName).toBe('DIV')
-    expect(slideList[1]).toHaveClass('slide')
-
-  });
-
-});  
-
 describe("Common key handling", function() {
 	
   it("should simulate keydown", function() {
@@ -53,9 +18,60 @@ describe("Common key handling", function() {
   
 });
 
-describe("Common post and get", function() {
+describe("Common GET", function() {
+
+  it("should GET Synchronous", function() {
+
+    spyOn(XMLHttpRequest.prototype, 'open').andCallThrough()
+    spyOn(XMLHttpRequest.prototype, 'send')	  
+	  
+    getResponse = getResource('/teacher_current_slide')
+
+    expect(XMLHttpRequest.prototype.open).toHaveBeenCalled()
+    expect(XMLHttpRequest.prototype.open.calls.length).toBe(1)	  
+    expect(XMLHttpRequest.prototype.open).toHaveBeenCalledWith('GET', '/teacher_current_slide', SYNCHRONOUS)
+    expect(XMLHttpRequest.prototype.send).toHaveBeenCalled()  
+    expect(XMLHttpRequest.prototype.send.calls.length).toBe(1)
+    expect(getResponse).not.toBeUndefined()
+	  
+  });  
   
-  it("should post ASynchronous", function() {
+  it("should concatenate url with global variable SERVER_PATH", function() {
+
+    spyOn(XMLHttpRequest.prototype, 'open').andCallThrough()
+    spyOn(XMLHttpRequest.prototype, 'send')	     
+    
+    SERVER_PATH = '//serveur'
+    
+    getResponse = getResource('/teacher_current_slide')
+    expect(XMLHttpRequest.prototype.open).toHaveBeenCalledWith('GET', SERVER_PATH + '/teacher_current_slide', SYNCHRONOUS)    
+    
+    SERVER_PATH = ''    
+    
+  });  
+
+});
+
+describe("Common POST", function() {
+  
+  it("should POST Synchronous", function() {
+
+    spyOn(XMLHttpRequest.prototype, 'open').andCallThrough()
+    spyOn(XMLHttpRequest.prototype, 'send')
+	  
+    postResponse = postResource('/teacher_current_slide', "1", SYNCHRONOUS)
+
+    expect(XMLHttpRequest.prototype.open).toHaveBeenCalled()
+    expect(XMLHttpRequest.prototype.open.calls.length).toBe(1)
+    expect(XMLHttpRequest.prototype.open).toHaveBeenCalledWith('POST', '/teacher_current_slide', SYNCHRONOUS)
+    expect(XMLHttpRequest.prototype.send).toHaveBeenCalled()
+    expect(XMLHttpRequest.prototype.send).toHaveBeenCalledWith("1")
+    expect(XMLHttpRequest.prototype.send.calls.length).toBe(1)	  
+    expect(postResponse).not.toBeUndefined()	  
+	  
+  });   
+  
+  it("should POST ASynchronous", function() {
 
     spyOn(XMLHttpRequest.prototype, 'open').andCallThrough()
     spyOn(XMLHttpRequest.prototype, 'send')
@@ -72,41 +88,12 @@ describe("Common post and get", function() {
 
   });
   
-  it("should get and post Synchronous", function() {
-
-    spyOn(XMLHttpRequest.prototype, 'open').andCallThrough()
-    spyOn(XMLHttpRequest.prototype, 'send')	  
-	  
-    getResponse = getResource('/teacher_current_slide')
-
-    expect(XMLHttpRequest.prototype.open).toHaveBeenCalled()
-    expect(XMLHttpRequest.prototype.open.calls.length).toBe(1)	  
-    expect(XMLHttpRequest.prototype.open).toHaveBeenCalledWith('GET', '/teacher_current_slide', SYNCHRONOUS)
-    expect(XMLHttpRequest.prototype.send).toHaveBeenCalled()  
-    expect(XMLHttpRequest.prototype.send.calls.length).toBe(1)
-    expect(getResponse).not.toBeUndefined()
-	  
-    postResponse = postResource('/teacher_current_slide', "1", SYNCHRONOUS)
-
-    expect(XMLHttpRequest.prototype.open).toHaveBeenCalled()
-    expect(XMLHttpRequest.prototype.open.calls.length).toBe(2)
-    expect(XMLHttpRequest.prototype.open).toHaveBeenCalledWith('POST', '/teacher_current_slide', SYNCHRONOUS)
-    expect(XMLHttpRequest.prototype.send).toHaveBeenCalled()
-    expect(XMLHttpRequest.prototype.send).toHaveBeenCalledWith("1")
-    expect(XMLHttpRequest.prototype.send.calls.length).toBe(2)	  
-    expect(postResponse).not.toBeUndefined()	  
-	  
-  });	  
-  
-  it("should get and post with global variable SERVER_PATH", function() {
+  it("should concatenate url with global variable SERVER_PATH", function() {
 
     spyOn(XMLHttpRequest.prototype, 'open').andCallThrough()
     spyOn(XMLHttpRequest.prototype, 'send')	     
     
     SERVER_PATH = '//serveur'
-    
-    getResponse = getResource('/teacher_current_slide')
-    expect(XMLHttpRequest.prototype.open).toHaveBeenCalledWith('GET', SERVER_PATH + '/teacher_current_slide', SYNCHRONOUS)    
     
     postResponse = postResource('/teacher_current_slide', "", SYNCHRONOUS)
     expect(XMLHttpRequest.prototype.open).toHaveBeenCalledWith('POST', SERVER_PATH + '/teacher_current_slide', SYNCHRONOUS)      
