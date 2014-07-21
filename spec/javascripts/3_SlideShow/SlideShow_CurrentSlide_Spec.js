fca704b66fd9d9bf7731e446119bbd5cfb5120b1
itarc <itarc@numericable.fr>
2014-07-21 10:34:28 +0200

    Refactoring Current Slide Spec

--- spec/javascripts/3_SlideShow/SlideShow_CurrentSlide_Spec.js
+++ spec/javascripts/3_SlideShow/SlideShow_CurrentSlide_Spec.js
@@ -1,102 +1,80 @@
 describe("SlideShow Current Slide", function() {
   
   beforeEach(function () {
-    
-    setFixtures("<div class='slides'><div class='slide'/><div class='slide'/><div class='slide'/></div>")	  
-  
-  });
-
-  it("should be current position when slideshow is initialized", function() {
-    
-    getResource = jasmine.createSpy('getResource').andReturn('121;true');
-
-    var slideShow = new SlideShow(queryAll(document, '.slide'));
-    
-    expect(slideShow.position._currentIndex).toBe(121);
-    expect(slideShow.position._IDEDisplayed).toBe(true);
-
+    setFixtures("<div class='slides'><div class='slide'/><div class='slide'/><div class='slide'/></div>")	 
   });
   
   it("should be current position when position is refreshed", function() {
-    
     getResource = jasmine.createSpy('getResource').andReturn('0;false');
-
+    
     var slideShow = new SlideShow(queryAll(document, '.slide'));
-    
-    expect(slideShow.position._currentIndex).toBe(0);
-    expect(slideShow.position._IDEDisplayed).toBe(false);    
-    
     getResource = jasmine.createSpy('getResource').andReturn('212;true');
-    
     slideShow._refreshPosition();
     
     expect(slideShow.position._currentIndex).toBe(212);
     expect(slideShow.position._IDEDisplayed).toBe(true);
+  });  
 
+  it("should be current position when slideshow is initialized", function() {
+    getResource = jasmine.createSpy('getResource').andReturn('121;true');
+    
+    var slideShow = new SlideShow(queryAll(document, '.slide'));
+    
+    expect(slideShow.position._currentIndex).toBe(121);
+    expect(slideShow.position._IDEDisplayed).toBe(true);
   });
+
+  it("should be visible when slideshow initialized", function() {
+    getResource = jasmine.createSpy('getResource').andReturn('0;false');
+    
+    var slideShow = new SlideShow(queryAll(document, '.slide'))
+
+    expect(slideShow._slides[slideShow.position._currentIndex]._node.className).toBe('slide current');
+  });  
+  
+});  
+  
+describe("SlideShow Current Slide Navigation", function() {
+  
+  beforeEach(function () {
+    setFixtures("<div class='slides'><div class='slide'/><div class='slide'/><div class='slide'/></div>")	 
+    slideShow = new SlideShow(queryAll(document, '.slide'));
+  });  
 	
   it("should be next slide when next slide is called", function() {
-
-    var slideShow = new SlideShow([]);
-	  
     slideShow.position._currentIndex = 1;
     slideShow._numberOfSlides = 3;	  
 
     slideShow.next();
 
     expect(slideShow.position._currentIndex).toBe(2)
-
   });
   
-  it("should be previous slide when previous slide is called", function() {
-
-    var slideShow = new SlideShow([]);
-	  
+  it("should be previous slide when previous slide is called", function() {	  
     slideShow.position._currentIndex = 2;
     slideShow._numberOfSlides = 3;	  
 
     slideShow.prev()
 
     expect(slideShow.position._currentIndex).toBe(1)
-
   });
 
   it("should NOT be a slide beyond last slide", function() {
-
-    var slideShow = new SlideShow([]);
-	  
     slideShow.position._currentIndex = 2;
     slideShow._numberOfSlides = 3;
 
     slideShow.next()
 
     expect(slideShow.position._currentIndex).toBe(2)
-
   });
   
   it("should NOT be a slide under first slide", function() {
-
-    var slideShow = new SlideShow([]);
-	  
     slideShow.position._currentIndex = 0;
     slideShow._numberOfSlides = 3;
 
     slideShow.prev()
 
     expect(slideShow.position._currentIndex).toBe(0)
-
   });
-  
-  it("should be visible when slideshow initialized", function() {
-	  
-    setFixtures("<div class='slides'><div class='slide'/><div class='slide'/></div>")
-    
-    getResource = jasmine.createSpy('getResource').andReturn('0;false');
-    
-    var slideShow = new SlideShow(queryAll(document, '.slide'))
-
-    expect(slideShow._slides[slideShow.position._currentIndex]._node.className).toBe('slide current');
-
-  }); 
   
 });
