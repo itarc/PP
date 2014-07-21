15e2d29d08bdceca244b80a87a60495fdb182695
itarc <itarc@numericable.fr>
2014-07-21 11:58:05 +0200

    Refactoring Teache Navigation Spec

--- spec/javascripts/4_Teacher/Teacher_Navigation_Spec.js
+++ spec/javascripts/4_Teacher/Teacher_Navigation_Spec.js
@@ -1,129 +1,77 @@
-describe("TeacherSlideShow Navigation with 1 Slide", function() {
-
-  it("should have one current slide", function() {
-	  
-    setFixtures("<div class='slides'><div class='slide'></div></div>");
-    var teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'));
-
-    expect(teacherSlideShow._slides.length).toBe(1);	  
-    expect(teacherSlideShow._slides[0] instanceof Slide).toBe(true);
-
-    expect(teacherSlideShow._slides[0]._node.className).toBe('slide current');
-    expect(teacherSlideShow.position._currentIndex).toBe(0);
-
-  });
+describe("TeacherSlideShow Position (includes IDE)", function() {
   
-});
-
-describe("TeacherSlideShow Navigation with 2 Slides", function() {
-
-  it("should open on first slide", function() {
-	  
-    setFixtures("<div class='slides'><div class='slide'><div class='slide'></div></div>")
-    var teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'))
-
-    expect(teacherSlideShow._slides.length).toBe(2);	  
-    expect(teacherSlideShow._slides[0] instanceof Slide).toBe(true)
-    expect(teacherSlideShow._slides[1] instanceof Slide).toBe(true)
-
-    expect(teacherSlideShow.position._currentIndex).toBe(0)	  
-
-    expect(teacherSlideShow._slides[0]._node.className).toBe('slide current')
-    expect(teacherSlideShow._slides[1]._node.className).toBe('slide')
-
+  beforeEach(function() {
+    setFixtures(TEACHER_SLIDESHOW_WITH_IDE);
+    getResource = jasmine.createSpy('getResource').andReturn('0;false');
+    postResource = jasmine.createSpy('postResource');     
+    teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'))
+  }); 
+  
+  it("should be posted on server when slideshow initialized", function() {    
+    expect(postResource).toHaveBeenCalledWith('/teacher_current_slide', 'index=' + '0' + '&' + 'ide_displayed=false', ASYNCHRONOUS); 
   });  
   
-  it("should go to second slide", function() {
-	  
-    setFixtures("<div class='slides'><div class='slide'><div class='slide'></div></div>")
-    var teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'))
-
-    __triggerKeyboardEvent(document, RIGHT_ARROW)
-	  
-    expect(teacherSlideShow.position._currentIndex).toBe(1)	  
-	  
-    expect(teacherSlideShow._slides[0]._node.className).toBe('slide')
-    expect(teacherSlideShow._slides[1]._node.className).toBe('slide current')
-
-  });    
+  it("should be posted on server when teacher goes NEXT", function() {
+    teacherSlideShow.next(); 
+    
+    expect(postResource).toHaveBeenCalledWith('/teacher_current_slide', 'index=' + '1' + '&' + 'ide_displayed=false', ASYNCHRONOUS);     
+  });  
   
-  it("should go to second slide and go back to first slide", function() {
-	  
-    setFixtures("<div class='slides'><div class='slide'><div class='slide'></div></div>")
-    var teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'))
+  it("should be updated when teacher goes DOWN", function() {
+    teacherSlideShow.down();
+    
+    expect(postResource).toHaveBeenCalledWith('/teacher_current_slide', 'index=' + '0' + '&' + 'ide_displayed=true', ASYNCHRONOUS);         
+  });  
 
-    __triggerKeyboardEvent(document, RIGHT_ARROW)
-    __triggerKeyboardEvent(document, LEFT_ARROW)
-	  
-    expect(teacherSlideShow.position._currentIndex).toBe(0)	  
-	  
-    expect(teacherSlideShow._slides[0]._node.className).toBe('slide current')
-    expect(teacherSlideShow._slides[1]._node.className).toBe('slide')
-
-  }); 
-
-  it("should show first slide when home pressed", function() {
-
-    setFixtures("<div class='slides'><div class='slide'><div class='slide'></div></div>")
-    var teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'))
-
-    __triggerKeyboardEvent(document, RIGHT_ARROW)
-    __triggerKeyboardEvent(document, HOME)	  
-
-    expect(teacherSlideShow._slides[0]._node.className).toBe('slide current')
-    expect(teacherSlideShow._slides[1]._node.className).toBe('slide')	  
- 
-  });    
-
+  it("should be updated when teacher goes PREVIOUS", function() {   
+    teacherSlideShow.next();
+    teacherSlideShow.prev(); 
+    
+    expect(postResource).toHaveBeenCalledWith('/teacher_current_slide', 'index=' + '0' + '&' + 'ide_displayed=false', ASYNCHRONOUS);        
+  });   
+  
+  it("should be updated when teacher goes UP", function() {
+    teacherSlideShow.down();
+    teacherSlideShow.up();
+     
+    expect(postResource).toHaveBeenCalledWith('/teacher_current_slide', 'index=' + '0' + '&' + 'ide_displayed=false', ASYNCHRONOUS);      
+  });   
+  
 });
 
-describe("TeacherSlideShow Navigation with 3 Slides", function() {
+describe("TeacherSlideShow Navigation with 3 Slides (No IDE)", function() {
+  
+  beforeEach(function () {
+    setFixtures("<div class='slides'><div class='slide'><div class='slide'><div class='slide'></div></div>")
+    teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'))    
+  });  
 
   it("should open on first slide", function() {
-	  
-    setFixtures("<div class='slides'><div class='slide'><div class='slide'><div class='slide'></div></div>")
-    var teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'))
-	  
     expect(teacherSlideShow._slides.length).toBe(3);	  
 
     expect(teacherSlideShow._slides[0]._node.className).toBe('slide current')
     expect(teacherSlideShow._slides[1]._node.className).toBe('slide')	  
-    expect(teacherSlideShow._slides[2]._node.className).toBe('slide')	  
-	  
+    expect(teacherSlideShow._slides[2]._node.className).toBe('slide')
   })
   
   it("should go to second slide", function() {
-	  
-    setFixtures("<div class='slides'><div class='slide'><div class='slide'><div class='slide'></div></div>")
-    var teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'))
-
     __triggerKeyboardEvent(document, RIGHT_ARROW)
 	  
     expect(teacherSlideShow._slides[0]._node.className).toBe('slide')
     expect(teacherSlideShow._slides[1]._node.className).toBe('slide current')
     expect(teacherSlideShow._slides[2]._node.className).toBe('slide')	  
-
   }); 
 
   it("should go to third slide", function() {
-
-    setFixtures("<div class='slides'><div class='slide'><div class='slide'><div class='slide'></div></div>")
-    var teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'))
-
     __triggerKeyboardEvent(document, RIGHT_ARROW)
     __triggerKeyboardEvent(document, RIGHT_ARROW)
 	  
     expect(teacherSlideShow._slides[0]._node.className).toBe('slide')
     expect(teacherSlideShow._slides[1]._node.className).toBe('slide')
-    expect(teacherSlideShow._slides[2]._node.className).toBe('slide current')	  
-    
+    expect(teacherSlideShow._slides[2]._node.className).toBe('slide current')
   });   
 
   it("should go back to second slide", function() {
-
-    setFixtures("<div class='slides'><div class='slide'><div class='slide'><div class='slide'></div></div>")
-    var teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'))
-
     __triggerKeyboardEvent(document, RIGHT_ARROW)
     __triggerKeyboardEvent(document, RIGHT_ARROW)
     __triggerKeyboardEvent(document, LEFT_ARROW)
@@ -131,14 +79,9 @@ describe("TeacherSlideShow Navigation with 3 Slides", function() {
     expect(teacherSlideShow._slides[0]._node.className).toBe('slide')
     expect(teacherSlideShow._slides[1]._node.className).toBe('slide current')
     expect(teacherSlideShow._slides[2]._node.className).toBe('slide')		  
-    
   }); 
   
   it("should go back to first slide", function() {
-
-    setFixtures("<div class='slides'><div class='slide'><div class='slide'><div class='slide'></div></div>")
-    var teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'))
-
     __triggerKeyboardEvent(document, RIGHT_ARROW)
     __triggerKeyboardEvent(document, RIGHT_ARROW)
     __triggerKeyboardEvent(document, LEFT_ARROW)
@@ -147,14 +90,9 @@ describe("TeacherSlideShow Navigation with 3 Slides", function() {
     expect(teacherSlideShow._slides[0]._node.className).toBe('slide current')
     expect(teacherSlideShow._slides[1]._node.className).toBe('slide')
     expect(teacherSlideShow._slides[2]._node.className).toBe('slide')	  
-    
   })
   
   it("should NOT show last slide when arrow down pressed", function() {
-    
-    setFixtures("<div class='slides'><div class='slide'/><div class='slide'/></div>")
-    var teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'))
-	  
     expect(teacherSlideShow._slides[0]._node.className).toBe('slide current') 
     expect(teacherSlideShow.position._IDEDisplayed).toBe(false) 
 	  
@@ -171,24 +109,6 @@ describe("TeacherSlideShow Navigation with 3 Slides", function() {
   });  
   
 });
-
-HEADER = 
-"<div class='slide'/>"+"<section>"
-code_input = 
-"<textarea id='code_input'></textarea>"
-code_helpers = 
-"<div class='code_helper' id='code_helper_1'></div>"+
-"<div class='code_helper' id='code_helper_2'></div>"
-author_bar = 
-"<div class='code_author'><span id='author_name'></span></div>"
-buttons = 
-"<input type='button' id='execute'/>"+
-"<input type='button' id='send_code'/>"+
-"<input type='button' id='get_code'/>"
-code_ouput = 
-"<textarea id='code_output'></textarea>"
-FOOTER = 
-"</section>"+"</div>"
 
 TEACHER_SLIDESHOW_WITH_IDE =  
 "<div class='slides'>"+
@@ -207,17 +127,14 @@ code_ouput +
 FOOTER +
 "/div"
 
-describe("TeacherSlideShow Navigation With an IDE Slide", function() {
+describe("TeacherSlideShow Navigation With 3 Slides (includes IDE Slide)", function() {
   
   beforeEach(function() {
-  setFixtures(TEACHER_SLIDESHOW_WITH_IDE);
-
+    setFixtures(TEACHER_SLIDESHOW_WITH_IDE);
+    teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'))
   });  
 
-  it("should not go beyond penultimate slide", function() {
-	  
-    var teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'))
-	  
+  it("should not go beyond penultimate slide (2nd slide)", function() {
     expect(teacherSlideShow.position._currentIndex).toBe(0)	  
 	  
     __triggerKeyboardEvent(document, RIGHT_ARROW)
@@ -227,70 +144,24 @@ describe("TeacherSlideShow Navigation With an IDE Slide", function() {
     __triggerKeyboardEvent(document, RIGHT_ARROW)
 
     expect(teacherSlideShow.position._currentIndex).toBe(1)    
-	  
   });
   
-  it("should show IDE Slide when arrow down pressed", function() {
-    
-    var teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'))
+  it("should show IDE Slide when ARROW DOWN pressed", function() {
+    expect(teacherSlideShow._last_slide()._node.className).toBe('slide');
 	  
-    expect(teacherSlideShow._last_slide()._node.className).toBe('slide')  
+    __triggerKeyboardEvent(document, DOWN_ARROW);
 	  
-    __triggerKeyboardEvent(document, DOWN_ARROW)
-	  
-    expect(teacherSlideShow._last_slide()._node.className).toBe('slide current')    
-    
+    expect(teacherSlideShow._last_slide()._node.className).toBe('slide current');  
   });  
   
-  it("should show return to current slide when arrow up pressed", function() {
-    
-    var teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'))
+  it("should return to current slide when ARROW UP pressed", function() {
+    expect(teacherSlideShow._slides[0]._node.className).toBe('slide current'); 
 	  
-    expect(teacherSlideShow._slides[0]._node.className).toBe('slide current') 
+    __triggerKeyboardEvent(document, DOWN_ARROW);
+    expect(teacherSlideShow._slides[0]._node.className).toBe('slide');
+    __triggerKeyboardEvent(document, UP_ARROW);
 	  
-    __triggerKeyboardEvent(document, DOWN_ARROW)
-    
-    expect(teacherSlideShow._last_slide()._node.className).toBe('slide current') 
-    
-    __triggerKeyboardEvent(document, UP_ARROW)
-	  
-    expect(teacherSlideShow._slides[0]._node.className).toBe('slide current') 
-    
+    expect(teacherSlideShow._slides[0]._node.className).toBe('slide current');
   });
-  
-  it("should update position on server", function() {
-	  
-    getResource = jasmine.createSpy('getResource').andReturn('0;false');
-    postResource = jasmine.createSpy('postResource');
-       
-    spyOn(ServerExecutionContext.prototype, 'getContextOnServer').andReturn({author: '', code: ''});     
-    
-    var teacherSlideShow = new TeacherSlideShow(queryAll(document, '.slide'));
-    
-    expect(postResource).toHaveBeenCalledWith('/teacher_current_slide', 'index=' + '0' + '&' + 'ide_displayed=false', ASYNCHRONOUS); 
-    
-    postResource = jasmine.createSpy('postResource');
-    teacherSlideShow.next(); 
-    
-    expect(postResource).toHaveBeenCalledWith('/teacher_current_slide', 'index=' + '1' + '&' + 'ide_displayed=false', ASYNCHRONOUS);    
-
-    postResource = jasmine.createSpy('postResource');
-    teacherSlideShow.down();
-    
-    expect(postResource).toHaveBeenCalledWith('/teacher_current_slide', 'index=' + '1' + '&' + 'ide_displayed=true', ASYNCHRONOUS);        
-
-    postResource = jasmine.createSpy('postResource');
-    teacherSlideShow.prev(); 
-    
-    expect(postResource).toHaveBeenCalledWith('/teacher_current_slide', 'index=' + '0' + '&' + 'ide_displayed=true', ASYNCHRONOUS);     
-
-    postResource = jasmine.createSpy('postResource');
-    teacherSlideShow.up();
-     
-    expect(postResource).toHaveBeenCalledWith('/teacher_current_slide', 'index=' + '0' + '&' + 'ide_displayed=false', ASYNCHRONOUS);      
-
-  });  
   
 });
-
-
