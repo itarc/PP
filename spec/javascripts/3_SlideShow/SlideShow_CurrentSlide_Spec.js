c67481eb3c35e51aa83bcb0af8677b4167f15cfe
itarc <itarc@numericable.fr>
2014-07-22 10:29:32 +0200

    Removing TeacherPollSpec (useless)

--- spec/javascripts/3_SlideShow/SlideShow_CurrentSlide_Spec.js
+++ spec/javascripts/3_SlideShow/SlideShow_CurrentSlide_Spec.js
@@ -1,38 +1,52 @@
-describe("SlideShow Current Slide", function() {
+SLIDESHOW_WITH_3_SLIDES = 
+"<div class='slides'>"+
+  "<div class='slide'/>"+
+  "<div class='slide'/>"+
+  "<div class='slide'/>"+
+"</div>"
+
+describe("SlideShow Position", function() {
   
   beforeEach(function () {
-    setFixtures("<div class='slides'><div class='slide'/><div class='slide'/><div class='slide'/></div>")	 
+    setFixtures(SLIDESHOW_WITH_3_SLIDES)	 
   });
   
-  it("should be current position when position is refreshed", function() {
-    getResource = jasmine.createSpy('getResource').andReturn('0;false');
-    
-    var slideShow = new SlideShow(queryAll(document, '.slide'));
-    getResource = jasmine.createSpy('getResource').andReturn('212;true');
-    slideShow._refreshPosition();
-    
-    expect(slideShow.position._currentIndex).toBe(212);
-    expect(slideShow.position._IDEDisplayed).toBe(true);
-  });  
-
-  it("should be current position when slideshow is initialized", function() {
-    getResource = jasmine.createSpy('getResource').andReturn('121;true');
-    
+  it("should be position on server when slideshow is initialized", function() {
+    spyOn(Position.prototype, "getPosition").andReturn('121;true');
     var slideShow = new SlideShow(queryAll(document, '.slide'));
     
     expect(slideShow.position._currentIndex).toBe(121);
     expect(slideShow.position._IDEDisplayed).toBe(true);
+  });  
+  
+  it("should be position on server when position is refreshed", function() { 
+    var slideShow = new SlideShow(queryAll(document, '.slide'));
+    
+    expect(slideShow.position._currentIndex).toBe(0);
+    expect(slideShow.position._IDEDisplayed).toBe(false);
+    
+    spyOn(Position.prototype, "getPosition").andReturn('212;true');     
+    slideShow._refreshPosition();
+    
+    expect(slideShow.position._currentIndex).toBe(212);
+    expect(slideShow.position._IDEDisplayed).toBe(true);
+  });
+  
+}); 
+
+describe("SlideShow Current Slide", function() {
+  
+  beforeEach(function () {
+    setFixtures(SLIDESHOW_WITH_3_SLIDES)	 
   });
 
   it("should be visible when slideshow initialized", function() {
-    getResource = jasmine.createSpy('getResource').andReturn('0;false');
-    
     var slideShow = new SlideShow(queryAll(document, '.slide'))
 
     expect(slideShow._slides[slideShow.position._currentIndex]._node.className).toBe('slide current');
-  });  
-  
-});  
+  });
+
+}); 
   
 describe("SlideShow Current Slide Index", function() {
   
