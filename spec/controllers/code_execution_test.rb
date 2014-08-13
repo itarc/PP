a25c13a6356135c391308e58a8634606494e2bbc
itarc <itarc@numericable.fr>
2014-08-13 17:20:21 +0200

    removing undef methods

--- spec/controllers/code_execution_test.rb
+++ spec/controllers/code_execution_test.rb
@@ -5,8 +5,6 @@ require_relative "../../controllers/slideshow"
 require 'test/unit'
 require 'rack/test'
 
-
-
 class TestCodeRun < Test::Unit::TestCase
   
   include Rack::Test::Methods
@@ -33,20 +31,25 @@ class TestCodeRun < Test::Unit::TestCase
     post '/code_run_result/0', "print A"
     assert last_response.body.include?("uninitialized constant A")
   end
+
+  def test03_should_give_right_error_line_number
+    post '/code_run_result/0', "print A"
+    assert last_response.body.include?(":1:"), last_response.body
+  end
   
-  def test04_should_run_unit_tests
+  def test05_should_run_unit_tests
     post '/code_run_result/0', "require 'test/unit'"
     assert last_response.body.include?("0 tests, 0 assertions, 0 failures, 0 errors, 0 skips")  
   end
   
-  def test05_should_run_utf8_code
+  def test06_should_run_utf8_code
     post '/code_run_result/0', "puts 'éèêàâùï'"
     assert last_response.body.include?('invalid multibyte char (US-ASCII)') 	  
     post '/code_run_result/0', "#encoding: utf-8\nputs 'éèêàâùï'"
     assert_equal "éèêàâùï\n", last_response.body
   end
   
-  def test06_should_run_code_for_blackboard
+  def test07_should_run_code_for_blackboard
     post '/code_run_result_blackboard/0', "print 'b'"
     assert_equal "b", last_response.body    
   end    
