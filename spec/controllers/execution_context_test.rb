228013499fc576136c4cd04983f2d51222329c5d
itarc <itarc@numericable.fr>
2014-09-12 13:44:14 +0200

    split code execution spec

--- spec/controllers/execution_context_test.rb
+++ spec/controllers/execution_context_test.rb
@@ -4,65 +4,9 @@ require_relative "../../controllers/slideshow"
 
 require 'test/unit'
 require 'rack/test'
-
-class TestCodeRun < Test::Unit::TestCase
-  
-  include Rack::Test::Methods
-
-  def app
-    Sinatra::Application
-  end
-  
-  def setup
-    $db.execute_sql("delete from run_events")	  
-  end
-  
-  def test01_should_not_run_anything_when_no_code
-    post '/code_run_result', ""
-    assert_equal "", last_response.body
-  end
-  
-  def test02_should_run_code
-    post '/code_run_result', "print 2"
-    assert_equal "2", last_response.body
-  end
-
-  def test03_should_catch_exception
-    post '/code_run_result', "print A"
-    assert last_response.body.include?("uninitialized constant A")
-  end
-
-  def test03_should_give_right_error_line_number
-    post '/code_run_result', "print A"
-    assert last_response.body.include?(":1:"), last_response.body
-  end
-  
-  def test05_should_run_unit_tests
-    post '/code_run_result', "require 'test/unit'"
-    assert last_response.body.include?("0 tests, 0 assertions, 0 failures, 0 errors, 0 skips")  
-  end
-  
-  def test06_should_run_utf8_code
-    post '/code_run_result', "puts 'éèêàâùï'"
-    assert last_response.body.include?('invalid multibyte char (US-ASCII)') 	  
-    post '/code_run_result', "#encoding: utf-8\nputs 'éèêàâùï'"
-    assert_equal "éèêàâùï\n", last_response.body
-  end
-  
-  def test07_should_run_code_for_blackboard
-    post '/code_run_result_blackboard', "print 'b'"
-    assert_equal "b", last_response.body    
-  end    
-  
-  def teardown
-    $db.execute_sql("delete from run_events")	  
-  end
-  
-end
-
-
 require 'json'
-class TestSaveAndGetExecutionContext < Test::Unit::TestCase
+
+class TestExecutionContext < Test::Unit::TestCase
   
   include Rack::Test::Methods
 
@@ -109,7 +53,7 @@ class TestSaveAndGetExecutionContext < Test::Unit::TestCase
   
 end
 
-class TestLastExecution_in_teacher_slide < Test::Unit::TestCase
+class TestLastExecution < Test::Unit::TestCase
   
   include Rack::Test::Methods
 
